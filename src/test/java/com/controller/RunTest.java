afc70ef89ad916707d6369661e184d1b8bcd9b10
SCPC03 <SCPC03@SCPC03>
2017-11-06 17:53:40 +0800

    1

---
+++ src/test/java/com/controller/RunTest.java
@@ -0,0 +1,61 @@
+package com.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.entity.User;
+import com.service.DeptService;
+import com.service.PermissionService;
+import com.service.ResourcesService;
+import com.service.RoleService;
+import com.service.Role_permissionService;
+import com.service.UserService;
+import com.service.User_roleService;
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration({"classpath*:applicationContext-basic.xml"})  
+public class RunTest {
+	@Resource
+	private UserService userService;
+	@Resource
+	private DeptService deptService;
+	@Resource
+	private PermissionService permissionService;
+	@Resource
+	private ResourcesService resourcesService;
+	@Resource
+	private Role_permissionService role_permissionService;
+	@Resource
+	private RoleService roleService;
+	@Resource
+	private User_roleService user_roleService;
+	
+	@Test
+	public void index(){
+		User user=new User();
+		user.setAge(1);
+		user.setDept_id(1);
+		user.setName("1");
+		user.setPassword("1");
+		user.setRole_id(1);
+		user.setStatus(1);
+		user.setUsername("1");
+		user.setId(1);
+		userService.add(user);
+		User user2 = userService.getById(1);
+		System.out.println(user2.toString());
+		user2.setName("2");
+		userService.update(user2);
+		List<User> all = userService.getAll();
+		for (User user1 : all) {
+			System.out.println(user1);
+		}
+	}
+
+}
